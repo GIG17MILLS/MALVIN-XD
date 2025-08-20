f2ea801a892fbc384fdacb9bdcedede7e0b9b612
GIG17MILLS <g.i.g.sung2008@gmail.com>
2025-08-20 02:43:48 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~wzsm3QZK#sVVVxmQSADLbmLKhTLOkYlw2hvTgpgCs6mnnEvUPK-0",  add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "2348149589085", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
