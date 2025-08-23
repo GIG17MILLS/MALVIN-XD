c77e26a0c3b2b4d4f3b71423d15d56574f7e1a6d
GIG17MILLS <g.i.g.sung2008@gmail.com>
2025-08-23 08:50:12 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~tusHEIAT#Rnr5S4j6XaZRLSBkS4c7VS91kxtWXTFismp_EQbgRyo",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "2348149589085", // put your phone number here
   
-  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  PREFIX: ",", // prefix (e.g., ., /, !, *)
   
   TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
 };
